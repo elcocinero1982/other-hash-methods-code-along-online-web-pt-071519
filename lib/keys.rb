774a9803d5e6b9125158e5d529907c98d0873ee3
jose luis <cortezluisjose@gmail.com>
2019-07-31 23:28:10 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -14,6 +14,7 @@ def get_the_keys(groceries)
   vegetable: ["carrots", "broccoli", "cucumbers"],
   meat: ["chicken", "steak", "salmon"],
   grains: ["rice", "pasta"]
+  
 }#code your solution here!
 end
- groceries.values
+ 
